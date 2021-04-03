9824ac4b1090775e6e5713820e9aeddfaaccba6d
shouxingzi <79133015+shouxingzi@users.noreply.github.com>
2021-04-03 16:53:00 +0900

    バリテーションの設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,12 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  vlidates :nickname,        presence: true
+  vlidates :first_name,      presence: true
+  vlidates :last_name,       presence: true
+  vlidates :first_name_kana, presence: true
+  vlidates :last_name_kana,  presence: true
+  vlidates :birthday,        presence: true
+
 end
