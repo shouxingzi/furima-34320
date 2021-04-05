5a48dc8af949d112aa655e9d2d891099e53d22dc
shouxingzi <79133015+shouxingzi@users.noreply.github.com>
2021-04-05 19:22:59 +0900

    ActiveHash　アソシエーションの設定

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,10 @@
 class Product < ApplicationRecord
+  belongs_to :user
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :category
+  belongs_to_active_hash :postage
+  belongs_to_active_hash :prefecture
+  belongs_to_active_hash :shipping_day
+  belongs_to_active_hash :status
 end
