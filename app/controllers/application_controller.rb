2d0b92bdf9e467cd99766ba3e6c326fb99f2ba28
shouxingzi <79133015+shouxingzi@users.noreply.github.com>
2021-04-05 16:06:51 +0900

    ユーザー認証パスを環境変数に変更　不要なコメント削除

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,10 +6,10 @@ class ApplicationController < ActionController::Base
 
   def basic_auth
     authenticate_or_request_with_http_basic do |username, password|
-      username == 'shouxingzi' && password == 'gobukusai'
+      username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"]  # 環境変数を読み込む記述に変更
     end
   end
-
+    
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :first_name, :last_name, :first_name_kana, :last_name_kana, :birthday])
   end
