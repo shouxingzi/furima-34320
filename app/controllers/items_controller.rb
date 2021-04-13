38b6414b59adca87fc3022b5bd0685d944d9ca26
shouxingzi <79133015+shouxingzi@users.noreply.github.com>
2021-04-13 13:18:08 +0900

    アクセス制限の実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,6 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only:[:new]
+  before_action :authenticate_user!, only:[:new, :edit]
+  before_action :move_to_index, only: [:edit]
 
   def index
     @items = Item.all.order("created_at DESC")
@@ -38,6 +39,14 @@ class ItemsController < ApplicationController
   private
   def item_params
     params.require(:item).permit(:product_name, :description, :category_id, :status_id, :postage_id, :prefecture_id, :shipping_day_id, :price, :image).merge(user_id: current_user.id)                                  
+  end
+
+  def move_to_index
+    item = Item.find(params[:id])
+    user = item.user
+    if current_user != user
+      redirect_to action: :index
+    end
   end
 
 end
