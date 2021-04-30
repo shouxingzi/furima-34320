ad05068fdb662457b00f0c133fbd4198a0259d95
shouxingzi <79133015+shouxingzi@users.noreply.github.com>
2021-04-30 15:27:16 +0900

    画像プレビュー実装

--- app/javascript/tax_price.js
+++ app/javascript/tax_price.js
@@ -1,15 +1,16 @@
 
-const tax_price = function() {
-  const item_price = document.getElementById("item-price");
-  const add_tax_price = document.getElementById("add-tax-price");
-  const profit = document.getElementById("profit");
+document.addEventListener("DOMContentLoaded", () => {
+  if ( document.getElementById('item-price')){
+    const item_price = document.getElementById("item-price");
+    const add_tax_price = document.getElementById("add-tax-price");
+    const profit = document.getElementById("profit");
 
-  item_price.addEventListener('keyup', function() {
-    const tax = Math.floor(item_price.value * 0.1);
-    const profit_price = item_price.value - tax;
-    add_tax_price.innerHTML = tax;
-    profit.innerHTML = profit_price;
-  });
-}
+    item_price.addEventListener('keyup', function() {
+      const tax = Math.floor(item_price.value * 0.1);
+      const profit_price = item_price.value - tax;
+      add_tax_price.innerHTML = tax;
+      profit.innerHTML = profit_price;
+    });
+  }
+});
 
-window.addEventListener("load", tax_price);
